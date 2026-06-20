136194a81bf34ae406c99029d56d3969a7977081
Arjun c Anil <arjuncanil2@gmail.com>
2026-06-20 13:15:00 +0530

    chore: register signals in RunnerConfig.ready() and set verbose app name

--- runner/apps.py
+++ runner/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class RunnerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'runner'
+    verbose_name = 'Clutch Run'
+
+    def ready(self):
+        """
+        Import signal handlers when the app is fully loaded.
+        This ensures post_save / post_delete receivers are registered
+        before any model operations occur.
+        """
+        import runner.signals  # noqa: F401
