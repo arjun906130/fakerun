ef83b678dfc220070e551f69419c39aa79878a43
Arjun c Anil <arjuncanil2@gmail.com>
2026-07-06 09:20:28 +0530

    chore: register URL routes for player stats and reset score endpoints

--- runner/urls.py
+++ runner/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('api/submit-score/', views.submit_score, name='submit_score'),
     path('api/leaderboard/', views.get_leaderboard, name='get_leaderboard'),
+    path('api/player/<str:username>/stats/', views.get_player_stats, name='player_stats'),
+    path('api/player/<str:username>/reset/', views.reset_scores, name='reset_scores'),
 ]
